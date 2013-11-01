dd3456b1771ab04232fec88dabfaac825632bc14
Chetan Sarva <chetan@pixelcop.net>
2013-10-31 22:39:09 -0400

    fix sudoers for ubuntu 12+

--- packer/scripts/ubuntu/sudoers.sh
+++ packer/scripts/ubuntu/sudoers.sh
@@ -2,3 +2,4 @@
 
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
+sed -i -e 's/%sudo\s*ALL=(ALL:ALL) ALL/%sudo ALL=NOPASSWD:ALL/g' /etc/sudoers
